2e71fd80ce4ba1737e49c4336047bdbd319c63f8
ChrisDirkis <git@chrisdirk.is>
2020-12-05 12:33:10 +1000

    Template update

---
+++ template.py
@@ -0,0 +1,20 @@
+from itertools import *
+from functools import *
+from math import * 
+import re
+
+from typing import *
+
+input_file = "inputs/dayX"
+
+def part_1():
+    with open(input_file) as file:
+        pass
+
+def part_2():
+    with open(input_file) as file:
+        pass
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
